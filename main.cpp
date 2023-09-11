739aab443d7d30c216d9bd414770133453a6b857
enei <enei.sluga@gmail.com>
2023-09-11 16:17:55 +0200

    juhuhu

--- main.cpp
+++ main.cpp
@@ -100,6 +100,20 @@ openvdb::FloatGrid::Ptr convolveX(openvdb::FloatGrid::Ptr grid, vector<float> ke
         }
         output_grid.setValue(*a, acc);
     }
+    // tbb::task_scheduler_init init(tbb::task_scheduler_init::default_num_threads());
+    // auto output = grid->deepCopy();
+    // int size = kernel.size();
+    // int pol = -floor(size * 0.5);
+    // tbb::parallel_for_each(grid->evalActiveVoxelBoundingBox().beginXYZ(), [&](auto a) {
+    //     auto const_grid = grid->getConstAccessor();
+    //     auto output_grid = output->getAccessor();
+    //     float acc = 0;
+    //     for (int i = 0; i < size; i++) {
+    //         acc += kernel[i] * const_grid.getValue(*a + openvdb::v10_0::math::Coord(i + pol, 0, 0));
+    //     }
+    //     output_grid.setValue(*a, acc);
+        
+    // });
     return output;
 }
 
@@ -136,8 +150,8 @@ openvdb::FloatGrid::Ptr convolveZ(openvdb::FloatGrid::Ptr grid, vector<float> ke
 }
 
 openvdb::FloatGrid::Ptr DoG(openvdb::FloatGrid::Ptr grid) {
-    vector<float> small_gauss = {0.15338835280702454, 0.22146110682534667, 0.2503010807352574, 0.22146110682534667, 0.15338835280702454};
-    vector<float> big_gauss = {0.009300040045324049, 0.028001560233780885, 0.06598396774984912, 0.12170274650962626, 0.17571363439579307, 0.19859610213125314, 0.17571363439579307, 0.12170274650962626, 0.06598396774984912, 0.028001560233780885, 0.009300040045324049};
+    vector<float> small_gauss = {0.05399113, 0.24197145, 0.39894347, 0.24197145, 0.05399113};
+    vector<float> big_gauss = {0.0010282, 0.0075974, 0.03599435, 0.10934117, 0.21296753, 0.26596426, 0.21296753, 0.10934117, 0.03599435, 0.0075974, 0.0010282};
     auto small_blur = convolveZ(convolveY(convolveX(grid, small_gauss), small_gauss), small_gauss);
     auto big_blur = convolveZ(convolveY(convolveX(grid, big_gauss), big_gauss), big_gauss);
     
@@ -174,7 +188,7 @@ int main()
         {
             // Parse filename to get color
             vector<float> color = getModelColors(frame_dir.path());
-	    std::cout << getModelName(frame_dir.path()) << std::endl;
+        	    // std::cout << getModelName(frame_dir.path()) << std::endl;
 
             // Read file and make r, g, and b grids
             openvdb::io::File file(frame_dir.path());
@@ -207,11 +221,48 @@ int main()
         openvdb::FloatGrid::Ptr blue = combineGrids(blue_grids);
         openvdb::FloatGrid::Ptr alpha = combineGrids(alpha_grids);
 
+        // Change background
+        // float colour[4] = {0.1, 0.0, 0.3, 0.1};
+        // openvdb::FloatGrid::Accessor red_acc = red->getAccessor();
+        // openvdb::FloatGrid::Accessor green_acc = green->getAccessor();
+        // openvdb::FloatGrid::Accessor blue_acc = blue->getAccessor();
+        // openvdb::FloatGrid::Accessor alpha_acc = alpha->getAccessor();
+        // for (auto i = alpha->evalActiveVoxelBoundingBox().beginXYZ(); i; ++i) {
+        //     auto r = red_acc.getValue(*i);
+        //     auto g = green_acc.getValue(*i);
+        //     auto b = blue_acc.getValue(*i);
+        //     if (alpha_acc.getValue(*i) < 0.11) {
+        //         red_acc.setValue(*i, colour[0]);
+        //         green_acc.setValue(*i, colour[1]);
+        //         blue_acc.setValue(*i, colour[2]);
+        //         alpha_acc.setValue(*i, colour[3]);
+        //     }
+        // }
+
+        // Blur
+        // string frame_dir = output_dir + entry.path().filename().string();
+        // fs::create_directory(frame_dir);
+
+        // vector<float> gauss = {0.12098749, 0.17603576, 0.19947465, 0.17603576, 0.12098749};
+        // vdb2Raw(convolveZ(convolveY(convolveX(red, gauss), gauss), gauss), frame_dir + "/red");
+        // vdb2Raw(convolveZ(convolveY(convolveX(green, gauss), gauss), gauss), frame_dir + "/green");
+        // vdb2Raw(convolveZ(convolveY(convolveX(blue, gauss), gauss), gauss), frame_dir + "/blue");
+        // vdb2Raw(convolveZ(convolveY(convolveX(alpha, gauss), gauss), gauss), frame_dir + "/alpha");
+
+        // Detect surface
         string frame_dir = output_dir + entry.path().filename().string();
         fs::create_directory(frame_dir);
-        vdb2Raw(red, frame_dir + "/red");
-        vdb2Raw(green, frame_dir + "/green");
-        vdb2Raw(blue, frame_dir + "/blue");
-        vdb2Raw(alpha, frame_dir + "/alpha");
+        vdb2Raw(DoG(red), frame_dir + "/red");
+        vdb2Raw(DoG(green), frame_dir + "/green");
+        vdb2Raw(DoG(blue), frame_dir + "/blue");
+        vdb2Raw(DoG(alpha), frame_dir + "/alpha");
+
+        // Normal
+        // string frame_dir = output_dir + entry.path().filename().string();
+        // fs::create_directory(frame_dir);
+        // vdb2Raw(red, frame_dir + "/red");
+        // vdb2Raw(green, frame_dir + "/green");
+        // vdb2Raw(blue, frame_dir + "/blue");
+        // vdb2Raw(alpha, frame_dir + "/alpha");
     }
 }
